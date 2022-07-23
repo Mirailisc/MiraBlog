da4e8f60b24a6839b0085728b6e2156c56b75a46
Mirailisc <mirailisclm@gmail.com>
2022-07-23 13:39:50 +0700

    fix: redirect

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
     if (!user) {
       navigate("/sign-in")
     }
-  }, [])
+  }, [navigate, user])
 
   const renderNavbar = () => {
     if(location.pathname === '/sign-in') return null
